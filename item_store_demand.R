f248911a64592c1e8932d8d92e75525f624168b8
unknown <nucannon11@gmail.com>
2024-11-20 16:46:48 -0700

    fit an ARIMA model for two store item combos

--- item_store_demand.R
+++ item_store_demand.R
@@ -71,9 +71,11 @@ ggsave("C:/Users/Josh/Documents/stat348/item_store_demand/hw27.png")
 first_store_recipe <- recipe(sales~.,data=storeItem) %>%
   step_date(date, features="doy") %>%
   step_date(date, features="dow") %>%
+  step_date(date, features = "month") %>%
+  step_mutate(date_month = factor(date_month)) %>%
   step_mutate(date_dow = factor(date_dow)) %>%
   step_range(date_doy, min=0, max=pi) %>%
-  step_mutate(sinDOY=cos(date_doy)) %>%
+  step_mutate(cosDOY=cos(date_doy)) %>%
   step_rm(store,item)
 
 install.packages("ranger")
@@ -106,4 +108,119 @@ best_rf_model <- rf_CV_results %>%
   show_best(metric = "smape")
 
 best_rf_model
+
+## ARIMA model
+library(modeltime)
+library(timetk)
+
+## pick out my items
+storeItemTrain <- store_train %>%
+  filter(store==1,item==11)
+storeItemTest <- store_test %>%
+  filter(store==1,item==11)
+storeItemTrain2 <- store_train %>%
+  filter(store==2, item == 12)
+storeItemTest2 <- store_test %>%
+  filter(store==2, item == 12)
+
+# Define the ARIMA model
+arima_cv_split <- time_series_split(storeItemTrain, assess="3 months", cumulative = TRUE)
+arima_cv_split %>%
+  tk_time_series_cv_plan() %>%
+  plot_time_series_cv_plan(date, sales, .interactive=FALSE)
+
+# create a recipe
+arima_recipe <- recipe(sales~.,data=storeItemTrain) %>%
+  step_date(date, features="doy") %>%
+  step_date(date, features="dow") %>%
+  step_date(date, features = "month") %>%
+  step_rm(item,store)
+
+## define the ARIMA model
+arima_model <- arima_reg(seasonal_period=365,
+                         non_seasonal_ar=5,
+                         non_seasonal_ma=5,
+                         seasonal_ar=2,
+                         seasonal_ma=2,
+                         non_seasonal_differences=2,
+                         seasonal_differences=2
+                         ) %>%
+  set_engine("auto_arima")
+
+# put into a single workflow and fit to the training data
+arima_wf <- workflow() %>%
+  add_recipe(arima_recipe) %>%
+  add_model(arima_model) %>%
+  fit(data=training(arima_cv_split))
+
+# tune the models
+arima_cv_results <- modeltime_calibrate(arima_wf,
+                                  new_data = testing(arima_cv_split))
+
+## visualize the results
+p1 <- arima_cv_results %>%
+  modeltime_forecast(
+    new_data = testing(arima_cv_split),
+    actual_data = training(arima_cv_split)
+  ) %>%
+  plot_modeltime_forecast(.interactive=FALSE)
+
+## tune to whole dataset
+fullfit <- arima_cv_results %>%
+  modeltime_refit(data=storeItemTrain)
+
+## predict for the whole dataset
+p2 <- fullfit %>%
+  modeltime_forecast(
+    new_data = storeItemTest,
+    actual_data = storeItemTrain) %>%
+  plot_modeltime_forecast(.interactive=FALSE)
+
+# let's do this again for another store item combo
+# Define the ARIMA model
+arima_cv_split2 <- time_series_split(storeItemTrain2, assess="3 months", cumulative = TRUE)
+arima_cv_split2 %>%
+  tk_time_series_cv_plan() %>%
+  plot_time_series_cv_plan(date, sales, .interactive=FALSE)
+
+# create a recipe
+arima_recipe2 <- recipe(sales~.,data=storeItemTrain2) %>%
+  step_date(date, features="doy") %>%
+  step_date(date, features="dow") %>%
+  step_date(date, features = "month") %>%
+  step_rm(store, item)
+
+# put into a single workflow and fit to the training data
+arima_wf <- workflow() %>%
+  add_recipe(arima_recipe2) %>%
+  add_model(arima_model) %>%
+  fit(data=training(arima_cv_split2))
+
+# tune the models
+arima_cv_results2 <- modeltime_calibrate(arima_wf,
+                                        new_data = testing(arima_cv_split2))
+
+## visualize the results
+p3 <- arima_cv_results2 %>%
+  modeltime_forecast(
+    new_data = testing(arima_cv_split2),
+    actual_data = training(arima_cv_split2)
+  ) %>%
+  plot_modeltime_forecast(.interactive=FALSE)
+
+## tune to whole dataset
+fullfit <- arima_cv_results2 %>%
+  modeltime_refit(data=storeItemTrain2)
+
+## predict for the whole dataset
+p4 <- fullfit %>%
+  modeltime_forecast(
+    new_data = storeItemTest2,
+    actual_data = storeItemTrain2) %>%
+  plot_modeltime_forecast(.interactive=FALSE)
+
+## let's put the plots together
+plotly::subplot(p1,p3,p2,p4,nrows=2)
+
+  
 
